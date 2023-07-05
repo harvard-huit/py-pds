477377c631fb81e2cc128509c1c038d3de2419c9
JaZahn <jcleveng@fas.harvard.edu>
2023-07-05 12:07:52 -0400

    first

---
+++ pds/__version__.py
@@ -0,0 +1,14 @@
+"""Version details for simple-salesforce
+
+This file shamelessly taken from the simple-salesforce library, which in turn took it from the requests library"""
+
+__title__ = 'pds'
+__description__ = 'A basic PDS REST API client.'
+__url__ = 'https://github.huit.harvard.edu/HUIT/py-pds'
+__version__ = '0.1.0'
+__author__ = 'JaZahn Clevenger'
+__author_email__ = 'jcleveng@fas.harvard.edu'
+__license__ = 'MIT'
+__maintainer__ = 'JaZahn Clevenger'
+__maintainer_email__ = 'jcleveng@fas.harvard.edu'
+__keywords__ = 'python pds person data'
---
+++ setup.py
@@ -0,0 +1,35 @@
+"""Simple-Salesforce Package Setup"""
+
+import textwrap
+from pathlib import Path
+
+from setuptools import setup
+
+here = Path(__file__).parent
+
+about = {}
+exec((here / 'pds' / '__version__.py').read_text(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    maintainer=about['__maintainer__'],
+    maintainer_email=about['__maintainer_email__'],
+    packages=['pds', ],
+    url=about['__url__'],
+    license=about['__license__'],
+    description=about['__description__'],
+    long_description=textwrap.dedent((here / 'README.rst').read_text()),
+    long_description_content_type='text/x-rst',
+    # package_data={},
+    install_requires = [
+        'requests>=2.31.0',
+        'dotmap>=1.3.30'
+        ],
+    tests_require = [],
+    # test_suite = 'pds.tests',
+    keywords = about['__keywords__'],
+    classifiers = []
+)
